594421ebfc7bc3d2d1e27197cfca0d20d96cc5c4
DESKTOP-VKQL2HU\mbola <242362@edu.p.lodz.pl>
2023-04-19 19:31:21 +0200

    Added parts of Dane, Prezentacja and Logika

--- ProjektPW/ProjektPW/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using ProjektPW;
-
-Student student = new Student();
-student.FirstName = "A";
-student.LastName = "B";
-
-Console.WriteLine(student.GetFullName());
---
+++ ProjektPW/ProjektPW/ballLogic.cs
@@ -0,0 +1,82 @@
+﻿using System.ComponentModel;
+using Dane;
+
+namespace Logika
+{
+    public class BallLogic : INotifyPropertyChanged
+    {
+        private Ball ball;
+        public BallLogic(Ball ball)
+        {
+            this.ball = ball;
+            this.ball.PropertyChanged += Update;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private void Update(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(ball.X))
+            {
+                OnPropertyChanged(nameof(ball.X));
+            }
+            else if (e.PropertyName == nameof(ball.Y))
+            {
+                OnPropertyChanged(nameof(ball.Y));
+            }
+            else if (e.PropertyName == nameof(ball.Radius))
+            {
+                OnPropertyChanged(nameof(ball.Radius));
+            }
+        }
+
+        public int X
+        {
+            get { return ball.X; }
+            set
+            {
+                if (ball.X != value)
+                {
+                    ball.X = value;
+                    OnPropertyChanged(nameof(ball.X));
+                }
+            }
+        }
+
+        public int Y
+        {
+            get { return ball.Y; }
+            set
+            {
+                if (ball.Y != value)
+                {
+                    ball.Y = value;
+                    OnPropertyChanged(nameof(ball.Y));
+                }
+            }
+        }
+
+        public int Radius
+        {
+            get { return ball.Radius; }
+            set
+            {
+                if (ball.Radius != value)
+                {
+                    ball.Radius = value;
+                    OnPropertyChanged(nameof(ball.Radius));
+                }
+            }
+        }
+
+
+    }
+}
